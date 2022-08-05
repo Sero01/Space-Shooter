371e0627ed3d268feb6f0ff4f3d54002d31924a2
Parvez Ahmed <126ahmedparvez@gmail.com>
2022-08-05 11:37:07 +0530

    canvas with ship

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function gameStart() {
-    
-}
+var canvas = document.getElementById("gameScreen");
+var ctx = canvas.getContext("2d");
+
+var shipReady = false;
+var shipImage = new Image();
+shipImage.onload = function () {
+    shipReady = true;
+};
+shipImage.src = "images/ship.png";
