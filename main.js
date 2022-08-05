0e485b2fbf4d2eff82a6c4177e1d40f3a1f6fc9c
Parvez Ahmed <126ahmedparvez@gmail.com>
2022-08-06 01:11:52 +0530

    working w/o hearts

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //create canvas
 var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
+var ctx = canvas.getContext("2d", {alpha: false});
 canvas.width = 400;
 canvas.height = 710;
 document.getElementById("gameArea").appendChild(canvas);
@@ -11,9 +11,11 @@ ctx.fillStyle = "white";
 var shipImage = new Image();
 var bulletImage = new Image();
 var enemyImage = new Image();
+var lifeImage = new Image();
 shipImage.src = "images/ship.png";
 bulletImage.src = "images/bullet.png";
 enemyImage.src = "images/enemy.png";
+lifeImage.src = "images/lives.png";
 
 //create the ship game object
 var ship = {
@@ -30,6 +32,9 @@ var bullet = [];
 //create enemy game object
 var enemy = [];
 
+//create life object
+var life = [];
+
 //handle keyboard input
 var keysDown = {};
 // Check for keys pressed where key represents the keycode captured
@@ -39,16 +44,30 @@ addEventListener("keydown", function (event) {
 addEventListener("keyup", function (event) {
   delete keysDown[event.key];
 });
+addEventListener("click", function(){
+    enemy.y=0;
+});
+
+//draw lives
+for(let i=deathCount;i<4;i++){
+    life.push({
+        x: canvas.width - i*50,
+        y: 0,
+        width: 50,
+        height: 50});
+    }
+ life.forEach(function(lifeObject){
+    ctx.drawImage(lifeImage, lifeObject.x, lifeObject.y, lifeObject.width, lifeObject.height);
+ },this);
 
 //draw the image
 function load(){
      ctx.drawImage(shipImage, ship.x, ship.y, ship.width, ship.height);
-     ctx.drawImage(bulletImage, bullet.x,bullet.y,bullet.width, bullet.height);
 }
-
 //update the game every frame
 var frame = 0;
 var count = 0;
+var deathCount = 0;
 function update(){
     if(keysDown.ArrowLeft && ship.x > 0){
         ship.x -= ship.speed*0.02;
@@ -67,7 +86,7 @@ function update(){
             y: ship.y});
     }
 
-    //spawn enemies every 100 frames
+    //spawn enemies every 70 frames
     if(frame%70 == 0){
         enemy.push({
         width: 30,
@@ -80,37 +99,51 @@ function update(){
     bullet.forEach(function(bulletObject){
         bulletObject.y -= 10;
         ctx.drawImage(bulletImage, bulletObject.x,bulletObject.y,bulletObject.width, bulletObject.height);
-        if(bulletObject.y < 0){
+        if(bulletObject.y < 50){
             bullet.splice(bullet.indexOf(bulletObject),1);
         }
-    }, this);
+    }, this); 
 
+ 
     //update enemy position and delete those which go out of frame
     enemy.forEach(function(enemyObject){
         enemyObject.y += 1;
         ctx.drawImage(enemyImage, enemyObject.x, enemyObject.y, enemyObject.width, enemyObject.height);
         if(enemyObject.y > canvas.height){
             enemy.splice(enemy.indexOf(enemyObject),1);
+            //if enemy goes out of frame, increase death count
+            deathCount++;
+            life.forEach(function(lifeObject){
+                life.splice(life.indexOf(lifeObject),1);
+             },this);
         }
     }, this);
+
     //check for collision between bullet and enemy
     bullet.forEach(function(bulletObject){
         enemy.forEach(function(enemyObject){
             if(bulletObject.x > enemyObject.x-5 && bulletObject.x < enemyObject.x + enemyObject.width && bulletObject.y > enemyObject.y && bulletObject.y < enemyObject.y + enemyObject.height){
                 bullet.splice(bullet.indexOf(bulletObject),1);
                 enemy.splice(enemy.indexOf(enemyObject),1);
-                count+=1;
-                console.log(count);
+                count++;
             }
         }, this);
     } , this);
-    ctx.fillText("Score: " + count, 10, 50);
+    document.getElementById("score").innerHTML = "Score:" + count;
+
+    //delete all bullets and enemies when game is over
+    if(deathCount == 3){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillText("Game Over", canvas.width/2 - 50, canvas.height/2);
+    }
+    else{
+        window.requestAnimationFrame(main);
+    }
 }
 //main driver function
 var main = function(){  
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     update();
     load();
-    requestAnimationFrame(main);
 };
 main();
