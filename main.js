2c6da8fd31ae02a4d4566e0aaa3d37809a9baecb
Parvez Ahmed <126ahmedparvez@gmail.com>
2022-08-07 03:02:25 +0530

    v1.2 (with music, highscore)

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //create canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d", {alpha: false});
+var game;
 canvas.width = 400;
 canvas.height = 710;
 document.getElementById("gameArea").appendChild(canvas);
@@ -35,11 +36,9 @@ var bullet = [];
 var enemy = [];
 var enemy2 = [];
 
-//create life object
-var life = [];
-
 //handle keyboard input
 var keysDown = {};
+var keyPress = {};
 // Check for keys pressed where key represents the keycode captured
 addEventListener("keydown", function (event) {
   keysDown[event.key] = true;
@@ -51,27 +50,20 @@ addEventListener("click", function(){
     enemy.y=0;
 });
 
-//draw lives
-for(let i=deathCount;i<4;i++){
-    life.push({
-        x: canvas.width - i*50,
-        y: 0,
-        width: 50,
-        height: 50});
-    }
- life.forEach(function(lifeObject){
-    ctx.drawImage(lifeImage, lifeObject.x, lifeObject.y, lifeObject.width, lifeObject.height);
- },this);
-
 //draw the image
 function load(){
-     ctx.drawImage(shipImage, ship.x, ship.y, ship.width, ship.height);
+    ctx.drawImage(shipImage, ship.x, ship.y, ship.width, ship.height);
 }
+
 //update the game every frame
 var frame = 0;
 var count = 0;
 var deathCount = 0;
+var enemyspeedmodifier = 0;
 function update(){
+    if(game){
+        window.cancelAnimationFrame(game);
+    }
     if(keysDown.ArrowLeft && ship.x > 0){
         ship.x -= ship.speed*0.02;
     }
@@ -79,34 +71,45 @@ function update(){
         ship.x += ship.speed*0.02;
     }
 
+    //increase speed modifier every 200 frames
+    if(frame%200 == 0){
+        enemyspeedmodifier+=0.1;
+    }
+
     //shoot bullets every 20 frames
     frame++;
-    if(frame%20 == 0){
+    if(frame%20==0){
         bullet.push({
             width: 10,
             height: 10,
             x: ship.x+30,
             y: ship.y});
+            document.getElementById("shoot").play();
     }
 
     //spawn enemies every 70 frames
-    if(frame%70 == 0){
+    if(frame%(70) == 0){
         enemy.push({
         width: 30,
         height: 30,
-        speed:1,
+        speed:1+enemyspeedmodifier,
         x: Math.floor(Math.random()*(canvas.width-60)+15),
         y: 0});
     }
 
     //spawn big enemies every 150 frames
-    if(frame%150 == 0){
+    if(frame%(150) == 0){
         enemy.push({
         width: 50,
         height: 50,
-        speed: 2,
+        speed: 2+enemyspeedmodifier,
         x: Math.floor(Math.random()*(canvas.width-60)+15),
         y: 0});
+    }
+
+    //increase speed of enemies every 200 frames
+    if(frame%10 == 0){
+        enemy.speed=10;
     }
     //update bullet position and delete those which go out of frame
     bullet.forEach(function(bulletObject){
@@ -126,38 +129,60 @@ function update(){
             //if enemy goes out of frame, increase death count and remove a life
             deathCount++;
             if(deathCount==1){
-                document.getElementById("life1").remove();
+                document.getElementById("life1").style.visibility="hidden";
             }
             if(deathCount==2){
-                document.getElementById("life2").remove();
+                document.getElementById("life2").style.visibility="hidden";
             }
             if(deathCount==3){
-                document.getElementById("life3").remove();
+                document.getElementById("life3").style.visibility="hidden";
             }
         }
     }, this);
     
     //check for collision between bullet and enemy
+    var highscore = localStorage.getItem("highscore");
     bullet.forEach(function(bulletObject){
         enemy.forEach(function(enemyObject){
             if(bulletObject.x > enemyObject.x-5 && bulletObject.x < enemyObject.x + enemyObject.width && bulletObject.y > enemyObject.y && bulletObject.y < enemyObject.y + enemyObject.height){
                 bullet.splice(bullet.indexOf(bulletObject),1);
                 enemy.splice(enemy.indexOf(enemyObject),1);
                 count++;
+                //check for highscore
+                if(count>highscore){
+                    highscore = count;
+                    localStorage.setItem("highscore", highscore);
+                }
+                document.getElementById("explosion").play();
             }
         }, this);
     } , this);
+    document.getElementById("highscore").innerHTML = "HighScore:" + highscore;
     document.getElementById("score").innerHTML = "Score:" + count;
-
 
     //delete all bullets and enemies when game is over
     if(deathCount == 3){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillText("Game Over", canvas.width/2 - 50, canvas.height/2);
+        ctx.fillText("Game Over!", canvas.width/2 - 50, canvas.height/2);
+        ctx.fillText("Click Replay to Play Again", canvas.width/2 - 100, canvas.height/2+50);
     }
     else{
-        window.requestAnimationFrame(main);
+        game = window.requestAnimationFrame(main);
     }
+}
+//replay function
+function replay(){  
+    deathCount = 0;
+    ship.x = canvas.width/2 - 35;
+    ship.y = canvas.height - 80;
+    enemy.length = 0;
+    bullet.length = 0;
+    count = 0;
+    enemyspeedmodifier = 0;
+    document.getElementById("life1").style.visibility="visible";
+    document.getElementById("life2").style.visibility="visible";
+    document.getElementById("life3").style.visibility="visible";
+    game = window.requestAnimationFrame(main);
 }
 //main driver function
 var main = function(){  
@@ -165,4 +190,5 @@ var main = function(){
     update();
     load();
 };
+localStorage.setItem("highscore", highscore);
 main();
